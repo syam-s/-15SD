cbc469e8b99d1ef3e493a2cb38157623903ebf77
Willie Huey <whuey@ucdavis.edu>
2015-05-23 11:54:12 -0700

    more fixes

--- Save_Images.m
+++ Save_Images.m
@@ -4,7 +4,7 @@ for j = first:last %last_image
     image = imread(strcat(directory,'//',files(j).name));
     [im_height, im_width, im_color] = size(image);
     imshow(strcat(directory,'//',files(j).name));
-    viscircles(Circle_Estimation(j,1:2),Circle_Estimation(j,3),'LineStyle','-');
+    viscircles(Circle_Estimation(j,1:2),Circle_Estimation(j,3),'LineWidth',.5);
     print(strcat(directory,'_',sprintf('%05d',j)),'-dtiff');
     close all;
 end
